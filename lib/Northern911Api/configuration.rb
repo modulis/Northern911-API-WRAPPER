c54624563823fc8486a10a4731fa8d8eac84898f
CHABANON Julien <julien.chabanon@modulis.ca>
2017-04-21 16:06:31 -0400

    Implement auth

---
+++ lib/Northern911Api/configuration.rb
@@ -0,0 +1,13 @@
+module DIDWW
+  class Configuration
+    attr_accessor :auth_hash, :sandbox
+
+    def initialize(params = {})
+      vendor_code = params[:vendor_code]
+      soap_passcode = params[:soap_passcode]
+      gmdate = Time.now.strftime('%Y%m%d')
+      @auth_hash = Digest::MD5.hexdigest(vendor_code + soap_passcode + gmdate)
+      @sandbox = params[:sandbox] || true
+    end
+  end
+end
